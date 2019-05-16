cf9039a21fbd384fcf0f855b994e6ed740047ce4
Jamin-Deng <839778985@qq.com>
2019-05-16 12:06:19 +0800

    updata proxy

--- components/header.js
+++ components/header.js
@@ -4,15 +4,34 @@ const linkStyle = {
     marginRight: 15
 }
 
-const Header = () => (
-    <div>
-        <Link href="/">
-            <a style={linkStyle}>Home</a>
-        </Link>
-        <Link href="/about">
-            <a style={linkStyle}>About</a>
-        </Link>
-    </div>
-)
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        // 注意需要执行一次 res.json() 方法才能获取数据
+        fetch('/api/hi')
+            .then(res => res.json())
+            .then(data => {
+
+            })
+            .catch(e => console.log('err', e))
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <Link href="/">
+                    <a style={linkStyle}>Home</a>
+                </Link>
+                <Link href="/about">
+                    <a style={linkStyle}>About</a>
+                </Link>
+            </div>
+        )
+    }
+}
 
 export default Header;
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const next = require('next');
 
+//本地跨域重定向
+const devProxy = {
+    '/api': {
+      target: 'http://localhost:3721',
+      pathRewrite: { '^/api': '/api' },
+      changeOrigin: true
+    }
+}
+
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -8,6 +17,15 @@ const handle = app.getRequestHandler();
 app.prepare()
     .then(() => {
         const server = express();
+
+        // Set up the proxy.
+        if (dev && devProxy) {
+                const proxyMiddleware = require('http-proxy-middleware')
+                Object.keys(devProxy).forEach(function (context) {
+                server.use(proxyMiddleware(context, devProxy[context]))
+            })
+        }
+
         // 将被伪装过的url和真实的url匹配起来
         server.get('/p/:id', (req, res) => {
             const actualPage = '/post';
